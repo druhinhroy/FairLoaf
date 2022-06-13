0eb172950e34bd8e79617aec0fc3f7f00951f10c
Abhishek Shrestha <107154482+AbhishekS-06@users.noreply.github.com>
2022-06-13 10:07:30 -0500

    Add files via upload

---
+++ Internship Website/main.js
@@ -0,0 +1,12 @@
+// Toggle Menu Code
+var navLinks = document.getElementById("navLinks")
+
+function showMenu()
+{
+  navLinks.style.right = "0";
+}
+
+function hideMenu()
+{
+  navLinks.style.right = "-200px";
+}
