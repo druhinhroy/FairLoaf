b9bce25339c9c8d26f5f101ddf4675b34b1ac537
Abhishek Shrestha <theshresthaabhishek@gmail.com>
2022-06-24 10:16:46 -0500

    Update

--- Internship Website/main.js
+++ Internship Website/main.js
@@ -10,3 +10,21 @@ function hideMenu()
 {
   navLinks.style.right = "-200px";
 }
+
+/*function initMap()
+{
+  let mapOptions = 
+  {
+    center: new google.maps.LatLng("30.2672", "-97.7431"),
+    zoom: 8
+  }
+  let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  let markerOptions = 
+  {
+      position: new google.maps.LatLng("0", "0"),
+      map: map
+  }
+  let marker =  new google.maps.Marker(markerOptions);
+
+  map.data.loadGeoJson("fixedlocations.geojson")
+}*/
